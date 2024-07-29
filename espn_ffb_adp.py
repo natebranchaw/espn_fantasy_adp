35f1fc46ba07b0da659184fc5f630ad8481dc2db
natebranchaw <natebranchaw@gmail.com>
2024-07-29 14:46:41 -0500

    Modified to access ESPN API

--- espn_ffb_adp.py
+++ espn_ffb_adp.py
@@ -1,87 +1,46 @@
-import time 
 import pandas as pd 
-import numpy as np
-from selenium import webdriver 
-from selenium.webdriver import Chrome 
-from selenium.webdriver.chrome.service import Service 
-from selenium.webdriver.common.by import By 
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
+import requests
+import json
 
-def get_espn_fantasy_football_adp(pages_to_scrape:int):
-    '''Takes as input the number of pages you wish to scrape from ESPN's ADP (Average Draft Position) Live Results.
-    Returns a dataframe with the columns: Player, Team, Position, ADP, and average draft round for 12, 10, and 8 team leagues'''
+def get_espn_fantasy_football_adp(year, num_players = 500):
+    '''Accesses ESPN's API to retrieve fantasy football player rankings and ADP for PPR leagues
+       Takes as input a year which is the year you want data for and num_players which is the number of player you would like to retrieve the default value is 500'''
     
-    espn_url = 'https://fantasy.espn.com/football/livedraftresults'
-    player_list = []
-    team_list = []
-    pos_list = [] 
-    adp_list = []
-    page = 0
+    espn_url = 'https://lm-api-reads.fantasy.espn.com/apis/v3/games/ffl/seasons/{}/segments/1/leaguedefaults/3?scoringPeriodId=0&view=kona_player_info'.format(year)
+    ranking_list = []
     
-    #Define options for Chrome Driver
-    options = webdriver.ChromeOptions() 
-    options.add_argument('headless')#it's more scalable to work in headless mode 
-
-    #Normally, selenium waits for all resources to download 
-    #We don't need it as the page also populated with the running javascript code. 
-    options.page_load_strategy = 'none' 
-
-    #This returns the path web driver downloaded 
-    chrome_path = ChromeDriverManager().install() 
-    chrome_service = Service(chrome_path) 
-
-    #Pass the defined options and service objects to initialize the web driver 
-    driver = Chrome(service = chrome_service, options = options) 
-    driver.implicitly_wait(10)
-    driver.get(espn_url)
+    filters = { "players": { "limit": 500, "sortDraftRanks": { "sortPriority": 100, "sortAsc": True, "value": "PPR" } } }
+    headers = {'x-fantasy-filter': json.dumps(filters)}
+    
+    r = requests.get(espn_url, headers = headers).json()
    
-    time.sleep(2)
-    
-    #Find the next button to advance to the next page of data
-    next_page_button = driver.find_element(By.XPATH, '//*[@id="fitt-analytics"]/div/div[5]/div[2]/div[2]/div/div/div/div[3]/nav/button[2]')
-
-    #Loop through the first seven pages of projections
-    while page <= pages_to_scrape:
+    for player in r['players']:
+        player_name = player['player']['fullName']
+        position = player['player']['defaultPositionId']
+        rank = player['player']['draftRanksByRankType']['PPR']['rank']
+        adp = player['player']['ownership']['averageDraftPosition']
         
-        adp_table = driver.find_element(By.XPATH, '//*[@id="fitt-analytics"]/div/div[5]/div[2]/div[2]/div/div/div/div[3]').get_attribute('outerHTML')
-        soup = BeautifulSoup(adp_table, 'html')
-
-        #Loop over all span elements with the class truncate for player names and append to player list
-        for player in soup.find_all('span', {'class': 'truncate'}):
-            if len(player.text) > 0:
-                player_list.append(player.text)
-        
-        #Loop over all span elements with the class playerinfo__playerteam for team names and append to team list
-        for team in soup.find_all('span', {'class': 'playerinfo__playerteam'}):
-            team_list.append((team.text).upper())
-        
-        #Loop over all span elements with the class playerinfo__playerpos ttu for player positons and append to pos list
-        for pos in soup.find_all('span', {'class': 'playerinfo__playerpos ttu'}):
-            pos_list.append(pos.text.upper())
-        
-        #Loop over all span elements with the class jsx-2810852873 table--cell sortedAscending adp tar sortable for player adp and append to adp list
-        for adp in soup.find_all('div', {'class': 'jsx-2810852873 table--cell sortedAscending adp tar sortable'}):
-            adp_list.append(adp.text)
+        if position == 1:
+            position = 'QB'
+        elif position == 2:
+            position = 'RB'
+        elif position == 3:
+            position = 'WR'
+        elif position == 4:
+            position = 'TE'
+        elif position == 16:
+            position = 'D/ST'
+        elif position == 5:
+            position = 'K'
+        else:
+            position = 'No Position'
             
-        #Click on the next page arrow
-        next_page_button.click()
+        temp_dict = {'Player': player_name, 'Position': position, 'Rank': rank, 'ADP': adp}
+        temp_df = pd.DataFrame([temp_dict])
+        ranking_list.append(temp_df)
         
-        #Increase the page to the next loaded page
-        page = page + 1
-        
-        time.sleep(4)
-        
-    driver.quit()
+    espn_rankings = pd.concat(ranking_list)
     
-    #Combine lists to create final dataframe
-    adp_df = pd.DataFrame({'Player': player_list, 'Team': team_list, 'Position': pos_list, 'ADP': adp_list})
-    adp_df['ADP'] = adp_df['ADP'].astype('float')
-    
-    #Create average round drafted for 12, 10, and 8 team leagues
-    adp_df['Round-12'] = np.ceil(adp_df['ADP'] / 12)
-    adp_df['Round-10'] = np.ceil(adp_df['ADP'] / 10)
-    adp_df['Round-8'] = np.ceil(adp_df['ADP'] / 8)
-        
-    return adp_df    
+    return espn_rankings
+
+x = get_espn_fantasy_football_adp(2024)
